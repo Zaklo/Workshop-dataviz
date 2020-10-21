b34dcc6daee036c6632c5546b0c1b0a6ce2462cf
Antoine Lozach <antoine.lozach@radiofrance.com>
2020-10-21 16:51:39 +0200

    fade on dates

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ let titles = [];
 let formatedMovies = []
 let myFont;
 let MAX_LENGTH_TITLE
+let angle, r, step, angle1, min = 0, max = 150;
+let tabMois = ['juillet', 'juin', 'mai', 'avril', 'mars', 'février', 'janvier', 'décembre', 'novembre', 'octobre', 'septembre', 'août']
+let positionAnne = []
+let pos = 7000
+let cam, alphaColor = 0;
 
 function preload() {
 
@@ -16,9 +21,10 @@ function preload() {
 }
 
 function setup() {
-    textSize(10)
-    background(123);
     createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+    cam = createCamera();
+    cam.move(0, 0, 7000)
+
     let filterMovies = Object.values(movies).filter((entry) => entry.release_date)
     let sortedMovies = filterMovies.sort((a, b) => new Date(a.release_date) < new Date(b.release_date))
 
@@ -46,43 +52,72 @@ function setup() {
         if (MAX_LENGTH_TITLE < temp || MAX_LENGTH_TITLE === undefined) MAX_LENGTH_TITLE = temp
     }
 
+    let startDate = 2015;
 
+    for (let i = 0; i < formatedMovies.length; i++) {
+        if (formatedMovies[i].date.substr(0, 4) == startDate) {
+            positionAnne.push(i - 1)
+            startDate -= 1
+        }
+    }
 }
 
 function draw() {
     smooth()
     background('#0c0c0c');
-    curveDetail(20)
-    orbitControl(0.5, 0.5);
-    strokeWeight(2)
-
-    fill(255, 30)
-    stroke(255, 50)
-
-    drawCircleGraph(0, 150, 0)
-    drawCircleGraph(150, 300, 2000)
-    drawCircleGraph(300, 450, 4000)
-
-    /*for (let i = 0; i < 150; i++) {
-        let nbTitles = formatedMovies[i].titles.length
-        let titre = formatedMovies[i].titles[0]
-        let x = map(i,0,149,-width,width)
-        let y = map(nbTitles, 1, 10, 0, height * .3)
-        //textFont(myFont);
-        //text(titre,x,y)
-    }*/
+    stroke(255)
+    noFill()
+    orbitControl(.01, .01, .0001)
+    drawCircleGraph(0, positionAnne[0], 0, "2016")
+    drawCircleGraph(positionAnne[0], positionAnne[1], -1300, "2015")
+    drawCircleGraph(positionAnne[1], positionAnne[2], -2600, "2014")
+    drawCircleGraph(positionAnne[2], positionAnne[3], -3900, "2013")
+    drawCircleGraph(positionAnne[3], positionAnne[4], -5100, "2012")
+    drawCircleGraph(positionAnne[4], positionAnne[5], -6400, "2011")
 }
 
-function drawCircleGraph(min, max, z) {
+function drawCircleGraph(min, max, z, annee) {
 
+    let mx = 8800;
+    let mn = 6200;
+    alphaColor = ((pos + z) - mn) / (mx - mn) * 255
+    fill(255, alphaColor)
+
+
+    textFont(myFont)
+    push()
+    translate(0, 0, z + pos)
+    textSize(50)
+    text(annee, -60, -80)
+    pop()
+
+    noFill()
     beginShape();
     for (let i = min; i < max; i++) {
         let nbTitles = formatedMovies[i].titles.length
         angle = map(i, min, max - 1, 0, TWO_PI)
-        let R = map(nbTitles, 1, MAX_LENGTH_TITLE, 300, 1000)
+        let R = map(nbTitles, 1, MAX_LENGTH_TITLE, 400, 1000)
         let x = R * cos(angle)
         let y = R * sin(angle)
-        curveVertex(x, y, z)
+        vertex(x, y, z + pos)
     }
     endShape(CLOSE);
+}
+
+function drawCalendar() {
+    r = 800;
+    angle1 = 0;
+    step = TWO_PI / 12;
+    for (let i = 0; i < 12; i++) {
+        let x = r * sin(angle1);
+        let y = r * cos(angle1);
+        fill('white');
+        textFont(myFont)
+        text(tabMois[i].toUpperCase(), x, y);
+        angle1 = angle1 + step;
+    }
+}
+
+function mouseWheel(event) {
+    pos += event.delta;
 }
